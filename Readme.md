115e7556bbf8b17d4356ef837dcdb8a291a7fffd
GabPhoenix <anjomito26@gmail.com>
2021-11-26 14:10:09 -0300

    connection

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# CONNECTION Java + PostgreSQL
+
+to use it, first configure postgre jar file in your project
+
+WARNING: AFTER CONNECTING, NEVER FORGET TO CLOSE THE CONNECTION!
---
+++ src/testesql1/Conexao.java
@@ -0,0 +1,66 @@
+package testesql1;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class Conexao {
+	private String url;
+	private String user;
+	private String password;
+	private Connection con;
+	
+//	Connection
+	Conexao(){
+		url = "jdbc:postgresql://localhost:5432/postgres";
+		user = "YOUR USER";
+		password = "YOUR PASSWORD";
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("CONNECTED SUCCESFULLY!");
+			
+			} catch (Exception e){
+			e.printStackTrace();
+			}
+			
+		}
+//	run commands
+	public int runSQL(String sql) {
+		try {
+			Statement stm = con.createStatement();
+			int res = stm.executeUpdate(sql);
+			con.commit();
+			return res;
+		}catch(Exception e){
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+//	search commands
+	public ResultSet searchSQL(String sql) {
+		try {
+			Statement stm = con.createStatement();
+			ResultSet rs = stm.executeQuery(sql);
+			return rs;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+//	close connection
+	public boolean closeConnection() {
+		try {
+			con.close();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+		
+}
